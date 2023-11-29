e5f19883708a8438fb593c411de9b413ad677a18
Aslon <aslon.dinov@gmail.com>
2023-11-29 12:28:43 +0100

    Änderung von mir nach Vinnie

--- src/main/java/Collobaration.java
+++ src/main/java/Collobaration.java
@@ -4,14 +4,12 @@ public class Collobaration {
         System.out.println("Hello, Group!");
 
         System.out.println("Hi ich bin Aslon ");
+
         System.out.println("Hi, ich bin Alex");
         System.out.println("Hi ich bin Jakob! Come and Code with me :D");
         System.out.println("Hi ich bin Vinnie. Convincing Git to do what we want :) ");
 
-    }
+        System.out.println("nächste Änderung von mir nach Vinnie ");
 
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
     }
 }
