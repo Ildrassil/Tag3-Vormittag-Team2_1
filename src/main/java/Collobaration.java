319f584e0af62d14c0917f5031d857db09b2e049
Alexandra Dernova <alexder@gmx.net>
2023-11-29 12:00:30 +0100

    Add Test Methode

--- src/main/java/Collobaration.java
+++ src/main/java/Collobaration.java
@@ -1,5 +1,6 @@
 public class Collobaration {
     public static void main(String[] args) {
         System.out.println("Hello, Grup!");
+        System.out.println("Hello, ich bin Alex!");
     }
 }
