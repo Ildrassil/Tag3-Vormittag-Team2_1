488d239e2096d9a7aecf7c9a19942bdcf10024d7
Ildrassil <schneiderjakob7@googlemail.com>
2023-11-29 12:24:44 +0100

    Add: Merged all Changes!

--- src/main/java/Collobaration.java
+++ src/main/java/Collobaration.java
@@ -7,5 +7,11 @@ public class Collobaration {
         System.out.println("Hi, ich bin Alex");
         System.out.println("Hi ich bin Jakob! Come and Code with me :D");
         System.out.println("Hi ich bin Vinnie. Convincing Git to do what we want :) ");
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
